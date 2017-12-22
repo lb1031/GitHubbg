6595477c396a34fa1de0a6f075ea3e3fde8c220d
liubin <491935574@qq.com>
2017-12-22 13:47:35 +0800

    1222 04commit

---
+++ backend/views/adminuser/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Adminuser */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="adminuser-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'age')->textInput() ?>
+
+    <?= $form->field($model, 'edu')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'pwd')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'creat_time')->textInput() ?>
+
+    <?= $form->field($model, 'update_time')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/adminuser/view.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Adminuser */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => '管理员管理', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="adminuser-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('修改', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => '你确定删除吗?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'name',
+            'age',
+            'edu',
+            'pwd',
+            'creat_time',
+            'update_time',
+        ],
+    ]) ?>
+
+</div>
